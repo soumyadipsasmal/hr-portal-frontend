ea0c26a1c6752521b00de7acc98aaa7965420c60
Soumyadip Sasmal <your-github-email@gmail.com>
2026-04-13 23:05:53 +0530

    add etc

--- suite.js
+++ suite.js
@@ -1,5 +1,5 @@
-// const API_BASE = "/api";
-const API_BASE = "http://3.111.186.50:5000/api";
+//  const API_BASE = "/api";
+const API_BASE = "http://13.204.61.85:5000/api";
 const APP_TODAY = "2026-04-02";
 const DEMO_NOW = "2026-04-02T09:40:00";
 const STATUS_LABELS = {
